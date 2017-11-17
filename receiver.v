847d5f3eb62851a53c19d2f468e308dba3fc9833
monoflo <lagasse.ryan.p@gmail.com>
2017-11-17 15:06:21 -0500

    Added project template

---
+++ receiver.v
@@ -0,0 +1,15 @@
+/*
+** ECE 353: Computer Systems Lab I
+** Lab 4: MIDI Receiver in Verilog
+**
+** Ryan Lagasse, Perveshwer Jaswal, Ricardo Henriquez
+*/
+
+module HalfAdder (A, B, S, C);
+
+endmodule
+
+module DFlipFlop (D, CLK, Q, Qnot);
+
+endmodule
+
