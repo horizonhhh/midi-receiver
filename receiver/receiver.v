2aa3c6afcf9355c75fd04caffe5135ffbef5561d
Ryan Lagasse <lagasse.ryan.p@gmail.com>
2017-11-28 21:14:58 -0500

    Progress on receiver implementation

--- receiver/receiver.v
+++ receiver/receiver.v
@@ -13,7 +13,50 @@ module receiver (CLK, DATA, RESET, LED);
 	input wire CLK, DATA, RESET;
 	output wire[7:0] LED;
 	
-	reg[7:0] note_number;
+	wire COUNT, SAMPLE;
+	wire[4:0] BIT;
+	wire[1:0] BYTE;
+	wire MSG;
+	wire[2:0] OVF;
+	
+	reg RECORD;
+	reg[7:0] note;
+	
+	timer t			(COUNT, RESET, SAMPLE, OVF[0]);
+	bit_state bis	(OVF[0], RESET, BIT, OVF[1]);
+	byte_state bys	(OVF[1], RESET, BYTE, OVF[2]);
+	msg_state ms	(OVF[2], RESET, MSG);
+	
+	assign COUNT = CLK & RECORD;
+	assign LED[7] = note[7] & MSG;
+	assign LED[6] = note[6] & MSG;
+	assign LED[5] = note[5] & MSG;
+	assign LED[4] = note[4] & MSG;
+	assign LED[3] = note[3] & MSG;
+	assign LED[2] = note[2] & MSG;
+	assign LED[1] = note[1] & MSG;
+	assign LED[0] = note[0] & MSG;
+	
+	always @(negedge DATA or posedge OVF[1])
+	begin
+		if (OVF[1]) RECORD <= 1'b0;
+		else if (RECORD == 1'b0) RECORD <= 1'b1;
+	end
+	
+	always @ (posedge SAMPLE)
+	begin
+		if (BIT != 5'h0 && BIT != 5'h9 && BYTE == 2'b1)
+		begin
+			note[7] <= note[6];
+			note[6] <= note[5];
+			note[5] <= note[4];
+			note[4] <= note[3];
+			note[3] <= note[2];
+			note[2] <= note[1];
+			note[1] <= note[0];
+			note[0] <= DATA;
+		end
+	end
 	
 endmodule
 
@@ -49,12 +92,10 @@ module bit_state (INC, RESET, STATE, OVF);
 	
 	parameter overflow = 5'h9;
 	
-	assign state_next = STATE + 1'b1;
-	
 	always @(posedge INC)
 	begin
 		if (STATE == overflow || !RESET) STATE <= 5'b0;
-		else STATE <= state_next;
+		else STATE <= STATE + 1;
 		
 		OVF <= STATE == 5'b0; // Fix w/ a non-blocking statement
 	end
