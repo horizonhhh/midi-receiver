971edcc62a4febdb161c8366214efcc9a210e699
Perveshwer <32558049+Perveshwer@users.noreply.github.com>
2017-11-30 20:53:48 -0500

    receiver.v

--- receiver/receiver.v
+++ receiver/receiver.v
@@ -10,33 +10,32 @@
 ***********************/
 
 module receiver (CLK, DATA, RESET, LED);
-	input wire CLK, DATA, RESET;
+	input CLK, DATA, RESET;
 	output wire[7:0] LED;
 	
-	wire COUNT, SAMPLE;
-	wire[4:0] BIT;
+	wire EN_BIT, EN_BYTE, EN_MSG;
+	wire SAMPLE;
+	wire[3:0] BIT;
 	wire[1:0] BYTE;
 	wire MSG;
-	wire[2:0] OVF;
 	
-	reg RECORD;
+	reg EN_CLK;
 	reg[7:0] note;
 	
-	timer t			(COUNT, RESET, SAMPLE, OVF[0]);
-	bit_state bis	(OVF[0], RESET, BIT, OVF[1]);
-	byte_state bys	(OVF[1], RESET, BYTE, OVF[2]);
-	msg_state ms	(OVF[2], RESET, MSG);
+	timer t			(CLK, EN_CLK, RESET, SAMPLE, EN_BIT);
+	bit_state bis	(CLK, EN_BIT, RESET, BIT, EN_BYTE);
+	byte_state bys	(CLK, EN_BYTE, RESET, BYTE, EN_MSG);
+	msg_state ms	(CLK, EN_MSG, RESET, MSG);
 	
-	assign COUNT = CLK & RECORD;
 	assign LED = note & { MSG, MSG, MSG, MSG, MSG, MSG, MSG, MSG };
 		
-	always @(negedge DATA or posedge OVF[1]) begin
-		if (OVF[1]) RECORD <= 1'b0;
-		else RECORD <= 1'b1;
+	always @(negedge DATA or posedge EN_BYTE) begin
+		if (EN_BYTE) EN_CLK <= 1'b0;
+		else EN_CLK <= 1'b1;
 	end
 	
-	always @ (posedge SAMPLE) begin
-		if (!MSG && BIT != 5'h0 && BIT != 5'h9 && BYTE == 2'b1) begin
+	always @(posedge SAMPLE) begin
+		if (!MSG && BIT != 4'h0 && BIT != 4'h9 && BYTE == 2'b1) begin
 			note[7] <= note[6];
 			note[6] <= note[5];
 			note[5] <= note[4];
@@ -53,82 +52,99 @@ endmodule
 **  Submodules  **
 ******************/
 
-module timer (CLK, RESET, SAMPLE, OVF);
-	input wire CLK, RESET;
-	output reg SAMPLE, OVF;
+module timer (CLK, EN, RESET, SAMPLE, OVF);
+	input wire CLK, EN, RESET;
+	output reg SAMPLE;
+	output reg OVF;
 		
 	reg[6:0] count;
 	
 	always @(posedge CLK or negedge RESET) begin
 		if (!RESET) begin
 			count <= 7'b0;
-			SAMPLE <= 1'b0;
-			OVF <= 1'b0;
+			//SAMPLE <= 1'b0;
+			//OVF <= 1'b0;
 		end
-		else begin
+		else if (EN) begin
 			count <= (count + 1'b1);
-			SAMPLE <= (count == 7'b1000000);
-			OVF <= (count == 7'b1111111);
+			//SAMPLE <= (count == 7'b1000000);
+			//OVF <= (count == 7'b1111111);
 		end
+	end
+	
+	always @(posedge count[6] or negedge RESET) begin
+		if (!RESET) SAMPLE <= 0;
+		else SAMPLE <= 1;
+	end
+	
+	always @(negedge count[6] or negedge RESET) begin
+		if (!RESET) OVF <= 0;
+		else OVF <= 1;
 	end
 endmodule
 
-module bit_state (INC, RESET, STATE, OVF);
-	input wire INC, RESET;
-	output reg[4:0] STATE;
+module bit_state (CLK, EN, RESET, STATE, OVF);
+	input wire CLK, EN, RESET;
+	output reg[3:0] STATE;
 	output reg OVF;
 	
 	parameter overflow = 5'h9;
 	
-	always @(posedge INC or negedge RESET) begin
+	always @(posedge CLK or negedge RESET) begin
 		if (!RESET) begin
-			STATE <= 5'b0;
-			OVF <= 0;
+			STATE <= 4'b0;
 		end
-		else begin
-			if (STATE == overflow) begin
-				STATE <= 0;
-				OVF <= 1;
-			end
-			else begin
-				STATE <= (STATE + 1'b1);
-				OVF <= 0;
-			end
+		else if (EN) begin
+			if (STATE == overflow) STATE <= 4'b0;
+			else STATE <= (STATE + 1'b1);
 		end
+	end
+	
+	always @(negedge STATE[3] or posedge STATE[0] or negedge RESET) begin
+		if (STATE[0] || !RESET) OVF <= 1'b0;
+		else OVF <= 1'b1;
 	end
 endmodule
 
-module byte_state (INC, RESET, STATE, OVF);
-	input wire INC, RESET;
+module byte_state (CLK, EN, RESET, STATE, OVF);
+	input wire CLK, EN, RESET;
 	output reg[1:0] STATE;
 	output reg OVF;
 	
 	parameter overflow = 2'h2;
 	
-	always @(posedge INC or negedge RESET) begin
-		if (!RESET) begin
-			STATE <= 2'b0;
-			OVF <= 0;
+	always @(posedge CLK or negedge RESET) begin
+		if (!RESET) STATE <= 2'b0;
+		else if (EN) begin
+			if (STATE == overflow) STATE <= 2'b0;
+			else STATE <= (STATE + 1'b1);
 		end
-		else begin
-			if (STATE == overflow) begin
-				STATE <= 0;
-				OVF <= 1;
-			end
-			else begin
-				STATE <= (STATE + 1'b1);
-				OVF <= 0;
-			end
-		end
+	end
+	
+	always @(negedge STATE[1] or posedge STATE[0] or negedge RESET) begin
+		if (STATE[0] || !RESET) OVF <= 1'b0;
+		else OVF <= 1'b1;
 	end
 endmodule
 
-module msg_state (INC, RESET, STATE);
-	input wire INC, RESET;
+module msg_state (CLK, EN, RESET, STATE);
+	input wire CLK, EN, RESET;
 	output reg STATE;
 	
-	always @(posedge INC or negedge RESET) begin
+	always @(posedge CLK or negedge RESET) begin
 		if (!RESET) STATE <= 1'b0;
-		else STATE <= (STATE + 1'b1);
+		else if (EN) STATE <= (STATE + 1'b1);
 	end
 endmodule
+
+
+
+
+
+
+
+
+
+
+
+
