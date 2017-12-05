709ae2a018d4c20802d79c65edf937f1d9c1ec6d
Ryan Lagasse <lagasse.ryan.p@gmail.com>
2017-12-05 12:58:03 -0500

    Upload receiver.v; missing from last commit

--- receiver/receiver.v
+++ receiver/receiver.v
@@ -9,23 +9,26 @@
 **  Receiver Module  **
 ***********************/
 
-module receiver (CLK, DATA, RESET, LED, CLK_EN, NOTE_SAMPLE, SAMPLE, BIT, BYTE, MSG);
+module receiver (CLK, DATA, RESET, LED);
 	input wire CLK, DATA, RESET;
 	output wire[7:0] LED;
 	
-	output reg CLK_EN;
-	output wire NOTE_SAMPLE;
+	reg CLK_EN;
+	wire NOTE_SAMPLE;
 	
-	output wire SAMPLE;
-	output wire[3:0] BIT;
-	output wire[1:0] BYTE;
-	output wire MSG;
+	wire SAMPLE;
+	wire[3:0] BIT;
+	wire[1:0] BYTE;
+	wire MSG;
+	
 	wire[7:0] NOTE;
+	
+	wire DISABLE;
 	
 	timer t	(CLK, CLK_EN, RESET, SAMPLE, BIT, BYTE, MSG);
 	note n	(CLK, NOTE_SAMPLE, DATA, RESET, NOTE);
 	
-	assign DISABLE = BIT[0] & BIT[3];
+	assign DISABLE = BIT[0] & BIT[3] & DATA;
 	assign NOTE_SAMPLE = SAMPLE && (BIT != 4'b0 && BIT != 4'b1001) && (BYTE == 2'b1) && !MSG;
 	assign LED = NOTE & { MSG, MSG, MSG, MSG, MSG, MSG, MSG, MSG };
 	
@@ -48,12 +51,13 @@ module timer (CLK, EN, RESET, SAMPLE, BIT, BYTE, MSG);
 	output reg[1:0] BYTE;
 	output reg MSG;
 	
+	parameter TIME_START = 7'b0000000;
 	parameter TIME_SAMPLE = 7'b1000000;
 	parameter OVF_TIME = 7'b1111111;
 	parameter OVF_BIT = 4'b1001;
 	parameter OVF_BYTE = 2'b10;
 	
-	always @(posedge CLK or negedge RESET) begin
+	always @(negedge CLK or negedge RESET) begin
 		if (!RESET) begin
 			TIME <= 7'b0;
 			BIT <= 4'b0;
@@ -62,30 +66,31 @@ module timer (CLK, EN, RESET, SAMPLE, BIT, BYTE, MSG);
 		end
 		else if (EN) begin
 			TIME <= TIME + 1'b1;
-	
+			
 			if (TIME == TIME_SAMPLE) SAMPLE <= 1;
 			else SAMPLE <= 0;
 			
 			if (TIME == OVF_TIME) BIT <= BIT + 1'b1;
 			
-			if (BIT == OVF_BIT) begin
-				BIT <= 4'b0;
-				BYTE <= BYTE + 1'b1;
-			end
-			
-			if (BYTE == OVF_BYTE) begin
-				BYTE <= 2'b0;
-				MSG <= MSG + 1'b1;
-			end
+			if (TIME == TIME_START) begin
+				if (BIT == OVF_BIT) begin
+					BIT <= 4'b0;
+					if (BYTE == OVF_BYTE) begin 
+						BYTE <= 2'b0;
+						MSG <= MSG + 1'b1;
+					end
+					else BYTE <= BYTE + 1'b1;
+				end
+			end			
 		end
 	end
 endmodule
 
 module note (CLK, SAMPLE, DATA, RESET, NOTE);
 	input wire CLK, DATA, SAMPLE, RESET;
-	output reg[7:0] NOTE;
+	output reg[0:7] NOTE;
 	
-	always @(posedge CLK or negedge RESET) begin
+	always @(negedge CLK or negedge RESET) begin
 		if (!RESET) NOTE <= 8'b0;
 		else if (SAMPLE) begin
 			NOTE[7] <= NOTE[6];
